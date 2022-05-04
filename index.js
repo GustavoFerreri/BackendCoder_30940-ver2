ed3225247b800cd2d7acc22122ecf7e4258fe4d9
Gustavo Ferreri <ferreri.gustavo@gmail.com>
2022-05-04 16:44:17 -0300

    Se implementa websocket con socket.io

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ require('dotenv').config();
 const app = require('./src/app');
 const { createConnection } = require('./src/database');
 const port = process.env.PORT || 8000;
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
+createConnection(); 
 
-createConnection();
-app.listen(port, ()=>{
+// Funcion con comillas
+io.on("connection", (socket)=>{
+    console.log('User Conected');
+    socket.on('dataClient', (data)=>{
+        msn.push(data);
+        io.sockets.emit('msjClient', msn)
+    });
+    socket.on('msjBack', (data)=>{
+        console.log(data)
+    })
+})
+
+server.listen(port, ()=>{
     console.log(`Servidor corriendo en http://localhost:${port}`);
 })
